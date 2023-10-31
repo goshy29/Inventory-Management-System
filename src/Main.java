1a68e490067bfdb7c2061ba4a7481c916b81b409
Georgi Dobromirov <georgi.dobromirov@gmail.com>
2023-10-31 19:18:37 +0200

    some optimizations on Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import itemtypes.ElectronicsItem;
-import itemtypes.FragileItem;
-import itemtypes.GroceryItem;
+import item_operations.AddItems;
+import item_operations.ReadItems;
+import item_operations.SortItems;
 import itemtypes.InventoryItem;
+import order.MakeOrder;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -10,18 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<InventoryItem> items = new ArrayList<>();
-        items.add(new ElectronicsItem("Laptop", "Electronics", "The best laptop ever",
-                true, false, 2000.00, 1, 100));
-        items.add(new ElectronicsItem("Monitor", "Electronics", "The best monitor ever",
-                true, false, 500.00, 2, 500));
-        items.add(new GroceryItem("Pasta", "Grocery", "The best pasta ever",
-                false, true, 20.00, 3, 1000));
-        items.add(new GroceryItem("Spaghetti", "Grocery", "The best spaghetti ever",
-                false, true, 15.00, 4, 1200));
-        items.add(new FragileItem("Porcelain", "Fragile", "The strongest porcelain ever",
-                false, true, 20.00, 5, 1000));
-        items.add(new FragileItem("Glass Cups", "Fragile", "The strongest cups ever",
-                false, true, 15.00, 6, 1200));
+        AddItems.addItemsToList(items);
         System.out.println("Welcome to the E-commerce Console Application!");
         boolean isRunning = true;
         while (isRunning) {
@@ -29,25 +19,21 @@ public class Main {
             int menuIndex = Integer.parseInt(sc.nextLine());
             switch (menuIndex) {
                 case 1:
-                    for (var item: items) {
-                        System.out.println(item.getItemDetails());
-                    }
+                    ReadItems.read();
                     menuIndex = backMenu();
                     if (menuIndex == 9){
                         isRunning = false;
                     }
                     break;
                 case 2:
-                    System.out.println("Enter Item id, name, category, description, isBreakable, isPerishable, price and quantity separated by comma!");
+                    System.out.println("Input Item id, name, category(Electronics, Grocery, Fragile), description,");
+                    System.out.println("isBreakable, isPerishable, price and quantity separated by comma!");
                     String[] input = sc.nextLine().trim().split(",");
                     System.out.println("8. Save");
                     System.out.println("9. Cancel");
                     menuIndex = Integer.parseInt(sc.nextLine());
                     if (menuIndex == 8) {
-                        if (input[2].equals("Electronics")) {
-                            items.add(new ElectronicsItem(input[1], input[2], input[3], Boolean.parseBoolean(input[4]),
-                                    Boolean.parseBoolean(input[5]), Double.parseDouble(input[6]), Integer.parseInt(input[0]), Integer.parseInt(input[7])));
-                        }
+                        AddItems.addNewItemToList(items, input);
                     }
                     menuIndex = backMenu();
                     if (menuIndex == 9){
@@ -59,27 +45,14 @@ public class Main {
                     System.out.println("2. Grocery Items");
                     System.out.println("3. Fragile Items");
                     menuIndex = Integer.parseInt(sc.nextLine());
-                    if (menuIndex == 1) {
-                        for (var item: items) {
-                            if (item.getCategory().equals("Electronics")) {
-                                System.out.println(item.getItemDetails());
-                            }
-                        }
-                    } else if (menuIndex == 2) {
-                        for (var item: items) {
-                            if (item.getCategory().equals("Grocery")) {
-                                System.out.println(item.getItemDetails());
-                            }
-                        }
+                    SortItems.sort(items, menuIndex);
+                    menuIndex = backMenu();
+                    if (menuIndex == 9){
+                        isRunning = false;
                     }
-                    else {
-                        for (var item: items) {
-                            if (item.getCategory().equals("Fragile")) {
-                                System.out.println(item.getItemDetails());
-                            }
-                        }
-                    }
-
+                    break;
+                case 4:
+                    MakeOrder.order(items);
                     menuIndex = backMenu();
                     if (menuIndex == 9){
                         isRunning = false;
@@ -97,9 +70,9 @@ public class Main {
     private static void displayMenu() {
         System.out.println("Menu:");
         System.out.println("1. List of Items");
-        System.out.println("2. Add Items to List");
+        System.out.println("2. Add Item to List");
         System.out.println("3. Show by Category");
-        System.out.println("4. Order");
+        System.out.println("4. Make Order");
         System.out.println("9. Exit");
     }
 
@@ -110,6 +83,5 @@ public class Main {
         int menuIndex = Integer.parseInt(sc.nextLine());
         return menuIndex;
     }
-
 }
 
